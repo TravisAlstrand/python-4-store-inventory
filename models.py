322e31dc5e89b07a69c6e28bdc346f2b4a6457be
Travis Alstrand <travisalstrand@gmail.com>
2022-12-01 16:33:40 -0800

    ruh roh

--- models.py
+++ models.py
@@ -12,10 +12,10 @@ class Product(Base):
   __tablename__ = 'products'
 
   product_id = Column(Integer, primary_key=True)
-  product_name = Column('Name', String)
-  product_price = Column('Price', Integer)
-  product_quantity = Column('Quantity', Integer)
-  date_updated = Column('Date Updated', Date)
+  product_name = Column(String)
+  product_price = Column(Integer)
+  product_quantity = Column(Integer)
+  date_updated = Column(Date)
 
   def __repr__(self):
     return f'''Name: {self.product_name} - Price: {self.product_price} - Quantity: {self.product_quantity} - Date Updated: {self.date_updated}'''
